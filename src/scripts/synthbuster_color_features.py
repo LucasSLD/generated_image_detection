a37fa989d3de7e3b0bf26562311b116d344d5a28
Saland Lucas <lucas.saland@univ-lille.fr>
2024-05-30 09:25:25 +0200

    refactor: regenerating features from scratch with CLIP extraction

--- src/scripts/synthbuster_color_features.py
+++ src/scripts/synthbuster_color_features.py
@@ -1,25 +1,31 @@
 import zipfile
 import sys
 sys.path.append("../tools")
-from utils import convert_to_jpg, remove_directory, get_histograms
+from utils import convert_to_jpg, remove_directory, extract_clip_features
 from constants import TMP_DIR
 from datasets import load_from_disk, Dataset, DatasetInfo
 from tqdm import tqdm
 from PIL import Image
 import argparse
 import numpy as np
+import open_clip
+import torch
 
 QUALITY = 40
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-b","--bins",type=int,required=True,help="Number of bins for the histograms used as color features")
 parser.add_argument("-o","--output",required=True,type=str,help="path of the output")
+parser.add_argument("-d","--device",required=False,default="0",type=str,help="which GPU to use '0' or '1'")
 args = parser.parse_args()
 
-data = load_from_disk("../../data/synthbuster_test")
 d = {"generator": [],"features":[],"label":[]}
-for key in data.column_names:
-    d[key] = data[key]
+
+device = "cuda:" + args.device if torch.cuda.is_available() else "cpu"
+
+model, _, preprocess = open_clip.create_model_and_transforms('hf-hub:laion/CLIP-ViT-L-14-DataComp.XL-s13B-b90K',device=device)
+
+model.eval()
 
 with zipfile.ZipFile("../../data/synthbuster/synthbuster.zip",mode="r") as zf:
     nb_files = len([item for item in zf.infolist() if not item.is_dir()])
@@ -34,7 +40,10 @@ with zipfile.ZipFile("../../data/synthbuster/synthbuster.zip",mode="r") as zf:
                     img_ycbcr = np.asarray(img_jpg.convert("YCbCr"))
                     cb, _ = np.histogram(img_ycbcr[:,:,1].flatten(),bins=args.bins)
                     cr, _ = np.histogram(img_ycbcr[:,:,2].flatten(),bins=args.bins)
-                    d["features"][i] += list(cb) + list(cr)
+                    clip_features = extract_clip_features(img_jpg,model,preprocess,device).flatten()
+                    d["features"].append(np.hstack((clip_features,cb,cr)))
+                    d["generator"].append(generator)
+                    d["label"].append("fake")
                     i += 1
                     bar.n = i
                     bar.refresh()
@@ -44,10 +53,13 @@ data = load_from_disk("../../data/big_QF_" + str(QUALITY))["test"].filter(lambda
 
 with tqdm(total=data.num_rows,desc="Real images processing") as bar:
     for j, e in enumerate(data):
+        d["generator"].append("null")
+        d["label"].append("real")
         img_ycbcr = np.asarray(e["image"].convert("YCbCr"))
         cb, _ = np.histogram(img_ycbcr[:,:,1].flatten(),bins=args.bins)
         cr, _ = np.histogram(img_ycbcr[:,:,2].flatten(),bins=args.bins)
-        d["features"][i] += list(cb) + list(cr)
+        clip_features = extract_clip_features(e["image"],model,preprocess,device).flatten()
+        d["features"].append(np.hstack((clip_features,cb,cr)))
         i += 1
         bar.n = j
         bar.refresh()
@@ -60,5 +72,5 @@ info = DatasetInfo(f"""
 
 data = Dataset.from_dict(d,info=info)
 data.save_to_disk(args.output)
-
+print("successfully saved the data at: ",args.output)
 remove_directory(TMP_DIR)
