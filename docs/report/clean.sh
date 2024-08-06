54e2b623791e97ade2e6f285fbcf5845e08d7bfe
LucasSLD <lucassld700@gmail.com>
2024-08-06 18:22:03 +0200

    feat: started slides

--- docs/report/clean.sh
+++ docs/report/clean.sh
@@ -2,4 +2,21 @@
 
 latexmk report.tex -c
 
-rm report.synctex.gz
+rm report.synctex.gz || true
+
+BASENAME="presentation"
+
+# Define the extensions you want to protect
+PROTECT_EXTENSIONS=("tex" "pdf")
+
+# Iterate over all files that start with the BASENAME
+for file in ${BASENAME}.*; do
+    # Get the file extension
+    ext="${file##*.}"
+    
+    # Check if the file's extension is not in the protected list
+    if [[ ! " ${PROTECT_EXTENSIONS[@]} " =~ " ${ext} " ]]; then
+        # If not protected, remove the file
+        rm "$file"
+    fi
+done
